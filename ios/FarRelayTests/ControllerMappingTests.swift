b6d52321ac75cdcc28aecf85c0fa285a9f8a9dc2
Sebastian Bustamante <62528239+sebb-7@users.noreply.github.com>
2026-09-24 08:10:00 -0400

    test: restore controller rotor persistence expectations

--- ios/FarRelayTests/ControllerMappingTests.swift
+++ ios/FarRelayTests/ControllerMappingTests.swift
@@ -404,16 +404,16 @@ final class ControllerMappingTests: XCTestCase {
 
         settings.moveQuickNavigationCategory(.editing, direction: -1)
         XCTAssertEqual(
-            Array(settings.draftProfile.quickNavigationOrder.prefix(4)),
-            [.quickBar, .profiles, .editing, .controllerMapping]
+            Array(settings.draftProfile.quickNavigationOrder.prefix(3)),
+            [.quickBar, .editing, .profiles]
         )
 
         settings.saveDraft()
         let reloaded = ControllerMappingSettings(defaults: defaults)
         XCTAssertEqual(reloaded.activeProfile.quickBar[1].id, firstQuickBarID)
         XCTAssertEqual(
-            Array(reloaded.activeProfile.quickNavigationOrder.prefix(4)),
-            [.quickBar, .profiles, .editing, .controllerMapping]
+            Array(reloaded.activeProfile.quickNavigationOrder.prefix(3)),
+            [.quickBar, .editing, .profiles]
         )
     }
 
